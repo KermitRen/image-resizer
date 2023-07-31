ec8774bc47617f1af175129a0c4e5ff9a3be2e0c
Frederik Baggesen <201804495@post.au.dk>
2023-07-31 18:58:05 +0200

    Transition to resize form

--- app/renderer.js
+++ app/renderer.js
@@ -6,3 +6,30 @@ function closeApp() {
 function minimizeApp() {
     electronAPI.minimize()
 }
+
+function selectImage(input) {
+    
+    //Error Handling
+    if (input.files.length < 1) {
+        console.error("No file selected")
+        return
+    }
+    
+    const file = input.files[0]
+    if(!isImage(file)) {
+        console.error("Please select an image")
+        return
+    }
+
+    //Update UI
+    document.getElementById("intro_box").className = "hidden"
+    document.getElementById("form_box").className = ""
+    document.getElementById("image_display").src = URL.createObjectURL(file)
+    document.getElementById("file_name").innerText = file.name
+    console.log(file)
+}
+
+function isImage(file) {
+    const validTypes = ["image/gif", "image/png", "image/jpeg"]
+    return validTypes.includes(file.type)
+}
